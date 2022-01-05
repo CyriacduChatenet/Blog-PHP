410a4f62f0b91d50c8d8a6c588fd0ff0966a027e
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-01-05 10:22:58 +0100

    connected to DB

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
     $title = "Home";
     require_once("components/header.php");
 ?>
-<main></main>
-    <section class="herobanner">
-        <h1 class="title">Bienvenu sur Coder blog</h1>
-    </section>
-</main>
-
+<section class="herobanner">
+    <h1 class="title">Bienvenu sur Coder blog</h1>
+</section>
+<div class="controls">
+    <a href="" class="cta-link"><button class="cta">Add post</button></a>
+</div>
+<section class="article-container"></section>
 <?php require_once('components/footer.php'); ?>
